a312514b794f430cec411749fc59fb1e5b02fa1f
Hien-Minh <htrinh@macalester.edu>
2024-07-15 00:27:29 +0700

    added 'Cell' class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
     win.draw_line(Line(Point(10, 10), Point(100, 100)), "red")
+    cell = Cell(True, False, True, False, 10, 100, 10, 100)
+    cell.draw(win.canvas)
     win.wait_for_close()
 
 
