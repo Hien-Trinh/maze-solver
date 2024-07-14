69d4bd4f7a0ed2c7e62fdda07f3b3909b86b8fc2
Hien-Minh <htrinh@macalester.edu>
2024-07-14 20:26:15 +0700

    added draw_line function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 10), Point(100, 100)), "red")
     win.wait_for_close()
     
 main()
