5cb636a50f36cbebc523a9db42a433d0003e42c3
Hien-Minh <htrinh@macalester.edu>
2024-07-14 22:24:56 +0700

    reformatted files

--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window():
     def __init__(self, width, height) -> None:
         self.root = Tk()
@@ -8,7 +9,7 @@ class Window():
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
-        
+
     def redraw(self):
         self.root.update()
         self.root.update_idletasks()
@@ -18,16 +19,18 @@ class Window():
         while self.running:
             self.redraw()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
     def close(self):
         self.running = False
 
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
 
 class Point():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
 
 class Line():
     def __init__(self, start, end) -> None:
@@ -35,4 +38,5 @@ class Line():
         self.end = end
 
     def draw(self, canvas, fill_color):
-        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+        canvas.create_line(self.start.x, self.start.y,
+                           self.end.x, self.end.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from graphics import Window, Point, Line
 
+
 def main():
     win = Window(800, 600)
     win.draw_line(Line(Point(10, 10), Point(100, 100)), "red")
     win.wait_for_close()
-    
+
+
 main()
